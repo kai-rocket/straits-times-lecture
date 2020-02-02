b182a8a82dfcc640c2c3c9ad2f468ed2f3967448
Kai Yuan Neo <kai@rocketacademy.co>
2020-02-03 00:40:00 +0800

    Updates

---
+++ precache-manifest.20c4391802b75487d26965351ec2a3e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671210fded9f67d092b8f4717f905fe0",
+    "url": "/straits-times-lecture/index.html"
+  },
+  {
+    "revision": "46f2779434a04ea8dfc9",
+    "url": "/straits-times-lecture/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "bbd9a3a93d4b66304608",
+    "url": "/straits-times-lecture/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46f2779434a04ea8dfc9",
+    "url": "/straits-times-lecture/static/js/2.7ce1e02f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/straits-times-lecture/static/js/2.7ce1e02f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbd9a3a93d4b66304608",
+    "url": "/straits-times-lecture/static/js/main.4fbb9a0f.chunk.js"
+  },
+  {
+    "revision": "c9e48b52be17dfc0c02b",
+    "url": "/straits-times-lecture/static/js/runtime-main.999c9c4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/straits-times-lecture/static/media/logo.5d5d9eef.svg"
+  }
+]);
